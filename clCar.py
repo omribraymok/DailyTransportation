ef1733ada549ff55a8e8b6c64eab1d5f33774602
omri braymok <omribraymok@gmail.com>
2020-10-26 13:48:54 +0200

    class car

--- clCar.py
+++ clCar.py
@@ -1,3 +1,19 @@
 class Car:
-    def __init__(self, Id, Type, Cost, CostPerMinute, Capacity, Wheelchair):
 
+    def __init__(self, id, car_type, driver_cost, cost_per_minute, capacity, wheelchair):
+        self.id = id
+        self.car_type = car_type
+        self.driver_cost = driver_cost
+        self.cost_per_minute = cost_per_minute
+        self.capacity = capacity
+        self.wheelchair = wheelchair
+
+    def calculat_cost(self, minutes):
+        return self.cost_per_minute * minutes + self.driver_cost
+
+
+car_1 = Car(123, 'minibus', 25, 14, 5, 'yes')
+car_2 = Car(122, 'car', 25, 7, 3, 'no')
+
+print(car_1.calculat_cost(5))
+print(car_2.calculat_cost(5))
