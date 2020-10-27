dd90e806db341d98db546539cfcfbaad2b80abd0
omri braymok <omribraymok@gmail.com>
2020-10-27 13:47:59 +0200

    class car update

--- clCar.py
+++ clCar.py
@@ -12,14 +12,19 @@ class Car:
 
         Car.num_of_cars += 1
 
-    def calculat_cost(self, minutes):
+    def calculate_cost(self, minutes):
         return self.cost_per_minute * minutes + self.driver_cost
 
+    def wheelchair_option(self):
+        return self.wheelchair
+
 print(Car.num_of_cars)
 
-car_1 = Car(123, 'minibus', 25, 14, 5, 'yes')
-car_2 = Car(122, 'car', 25, 7, 3, 'no')
+car_1 = Car(123, 'minibus', 25, 14, 5, True)
+car_2 = Car(122, 'car', 25, 7, 3, False)
 
-print(car_1.calculat_cost(5))
-print(car_2.calculat_cost(5))
+print(car_1.calculate_cost(5))
+print(car_2.calculate_cost(5))
 print(Car.num_of_cars)
+
+print(car_1.wheelchair_option())
