8b7350cfb8803bd7043cbc1118f14340573a0c13
omri braymok <omribraymok@gmail.com>
2020-10-27 17:01:19 +0200

    classes update

---
+++ clSchool.py
@@ -0,0 +1,10 @@
+class Child:
+
+    def __init__(self, id, name, address, contacts, v_time):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.contacts = contacts
+        self.v_time = v_time
+
+
