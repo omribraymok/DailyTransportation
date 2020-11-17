d1085a2c957cb1e1dda50dc96dc897b3280919a8
Matan Asoolin <matanpm@gmail.com>
2020-11-17 12:46:40 +0200

    refactor commit #2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import math
 
 
 # Calculation of Euclidean distance ("time travel")
-def calculat_time_a_to_b(stop_a, stop_b):
+def calculate_euclidean_dist(stop_a, stop_b):
     (x1, y1) = stop_a.split(',')
     (x2, y2) = stop_b.split(',')
     (x1, y1) = (int(x1), int(y1))
@@ -21,18 +21,18 @@ def calculat_time_a_to_b(stop_a, stop_b):
 
 
 # Calculation of time and cost
-def calculat(child_a, child_b, car):
-    time = calculat_time_a_to_b(child_a.address, child_b.address)
-    cost = car.driver_cost + (car.cost_per_minute * time)
-    return cost, time
+def calculate_time_cost(child_a, child_b, car):
+    time_of_path = calculate_euclidean_dist(child_a.address, child_b.address)
+    cost_of_path = car.driver_cost + (car.cost_per_minute * time)
+    return cost_of_path, time_of_path
 
 
 # Using openpyxl to writing to excel file
 # Give the location of the file
-loc = "Worksheet.xlsx"
+data_file = "Worksheet.xlsx"
 
 # To open Workbook
-excel_file = openpyxl.load_workbook(loc)
+excel_file = openpyxl.load_workbook(data_file)
 
 # To open sheets
 child_sheet = excel_file.worksheets[0]
@@ -45,16 +45,18 @@ for i in range(20):
 
 # Increases the  child table{
 tmp = [ws.tables for ws in excel_file.worksheets]
-tables = [{v.name: v} for t in tmp for v in t.values()]
+tables_in_sheet = [{v.name: v} for t in tmp for v in t.values()]
 
-for table in tables:
+for table in tables_in_sheet:
     if list(table.keys())[0] == 'Child':
         table['Child'].ref = 'A1:G21'
 # }
 
-excel_file.save(loc)
+excel_file.save(data_file)
 
-child_dic = {}
+dict_of_all_children = {}
+
+# get data from excel file
 for x in range(2, 22):
     cell_obj1 = child_sheet.cell(row=x, column=1)
     cell_obj2 = child_sheet.cell(row=x, column=2)
@@ -63,15 +65,16 @@ for x in range(2, 22):
     cell_obj5 = child_sheet.cell(row=x, column=5)
     cell_obj6 = child_sheet.cell(row=x, column=6)
     cell_obj7 = child_sheet.cell(row=x, column=7)
-    child_dic[x - 2] = Child(cell_obj1.value, cell_obj2.value, cell_obj3.value,
+    dict_of_all_children[x - 2] = Child(cell_obj1.value, cell_obj2.value, cell_obj3.value,
                              cell_obj4.value, cell_obj5.value, cell_obj6.value,
                              cell_obj7.value)
 for x in range(0, 20):
-    print(child_dic[x])
+    print(dict_of_all_children[x])
 
 # Reading from excel file from Car table
 cars = excel_file.worksheets[2]
-car_dic = {}
+dict_of_cars = {}
+#get cars data from data sheet
 for x in range(2, 5):
     cell_obj1 = cars.cell(row=x, column=1)
     cell_obj2 = cars.cell(row=x, column=2)
@@ -79,24 +82,24 @@ for x in range(2, 5):
     cell_obj4 = cars.cell(row=x, column=4)
     cell_obj5 = cars.cell(row=x, column=5)
     cell_obj6 = cars.cell(row=x, column=6)
-    car_dic[x - 2] = Car(cell_obj1.value, cell_obj2.value, cell_obj3.value,
+    dict_of_cars[x - 2] = Car(cell_obj1.value, cell_obj2.value, cell_obj3.value,
                          cell_obj4.value, cell_obj5.value, cell_obj6.value)
 for x in range(0, 3):
-    print(car_dic[x])
+    print(dict_of_cars[x])
 
 # Reading from excel file from School table
 schools = excel_file.worksheets[4]
-schools_dic = {}
+dict_of_school = {}
 for x in range(2, 3):
     cell_obj1 = schools.cell(row=x, column=1)
     cell_obj2 = schools.cell(row=x, column=2)
     cell_obj3 = schools.cell(row=x, column=3)
     cell_obj4 = schools.cell(row=x, column=4)
     cell_obj5 = schools.cell(row=x, column=5)
-    schools_dic[x - 2] = School(cell_obj1.value, cell_obj2.value, cell_obj3.value,
+    dict_of_school[x - 2] = School(cell_obj1.value, cell_obj2.value, cell_obj3.value,
                                 cell_obj4.value, cell_obj5.value)
 for x in range(0, 1):
-    print(schools_dic[x])
+    print(dict_of_school[x])
 
 # child_dic_a = {}
 # group_a = random.sample(list(child_dic), 7)
